df86c5e5ec1e98a7e5fd7a180289cb6ef82da3db
Raul Bravo <rbravo555@gmail.com>
2023-06-29 18:23:33 +0200

    cleaning

--- Example.py
+++ Example.py
@@ -1,13 +1,17 @@
 
 import numpy as np
 from scipy.special.orthogonal import p_roots
-from matplotlib import pyplot as plt
 from scipy.optimize import linprog
 from empirical_cubature_method import EmpiricalCubatureMethod
 from randomized_singular_value_decomposition import RandomizedSingularValueDecomposition
-from plot_scripts import plot_original_function_1D, plot_basis_functions_1D, plot_spartity_plot, plot_lp_sparsity,plot_ground_truth
+from plot_scripts import plot_original_function_1D, plot_basis_functions_1D, plot_sparsity, plot_lp_sparsity,plot_ground_truth
 
-def gauss_quad(M, min_, max_):
+
+
+
+def gauss_quad(M):
+    min_ = 0
+    max_ = 1
     [X, W] = p_roots(M)
     dx = max_- min_
     dxi = 2
@@ -15,6 +19,8 @@ def gauss_quad(M, min_, max_):
     X = ((max_ - min_)/2)*X + ((max_ + min_)/2)
 
     return X, W
+
+
 
 
 def get_list_of_arrays_sizes(list_of_np_arrays):
@@ -25,8 +31,10 @@ def get_list_of_arrays_sizes(list_of_np_arrays):
         size_2+= list_of_np_arrays[i].shape[1]
     return size_1, size_2
 
+
+
+
 def list_to_mat(list_of_np_arrays):
-    #generic but not efficient
     size_1, size_2= get_list_of_arrays_sizes(list_of_np_arrays)
     numpy_arr = np.zeros((size_1,size_2))
 
@@ -35,17 +43,16 @@ def list_to_mat(list_of_np_arrays):
     for i in range(len(list_of_np_arrays)):
         size_1_array = list_of_np_arrays[i].shape[0]
         size_2_array = list_of_np_arrays[i].shape[1]
-
         numpy_arr[gap_horizontal:gap_horizontal+size_1_array,gap_vertical:gap_vertical+size_2_array] = list_of_np_arrays[i]
-
         gap_horizontal += size_1_array
         gap_vertical +=size_2_array
-
     return numpy_arr
 
 
+
+
+
 def list_to_vec(list_of_np_arrays):
-    #hardcoded a bit
     size_2 = len(list_of_np_arrays)
     for i in range(size_2):
         if i==0:
@@ -55,24 +62,21 @@ def list_to_vec(list_of_np_arrays):
     return numpy_arr
 
 
+
+
+
 def local_ecm(Matrixlist, vectorlist, swap_functions, constrain_sum_of_weights ):
     Number_Of_Clusters = len(Matrixlist)
     z_i = []
     w_i = []
     z = None
-
     for i in range(Number_Of_Clusters):
         z_i.append([])
         w_i.append([])
-
     unsuccesfull_index = 0
-
     if swap_functions ==True:
         Matrixlist = Matrixlist[::-1]
-
     for i in range(Number_Of_Clusters):
-        print('current z: ', z)
-        print('current index : ', i)
         hyper_reduction_element_selector = EmpiricalCubatureMethod()
         uu, ss, vv, ee = RandomizedSingularValueDecomposition().Calculate(Matrixlist[i].T)
         hyper_reduction_element_selector.SetUp( vv.T, Weights=vectorlist,InitialCandidatesSet = z, constrain_sum_of_weights=constrain_sum_of_weights)  #add again z
@@ -86,22 +90,14 @@ def local_ecm(Matrixlist, vectorlist, swap_functions, constrain_sum_of_weights )
             hyper_reduction_element_selector.Calculate()
         w_i[i] = np.squeeze(hyper_reduction_element_selector.w)
         z_i[i] = np.squeeze(hyper_reduction_element_selector.z)
-
-        print(w_i[i])
-        print(z_i[i])
         if z is None:
             z = z_i[i]
         else:
             z = np.union1d(z,z_i[i])
-
     if swap_functions ==True:
         w_i = w_i[::-1]
         z_i = z_i[::-1]
-
     WeightsMatrix = np.zeros(( (Matrixlist[0].shape)[0],Number_Of_Clusters))
-    print('\n number of TOTAL elements: ',WeightsMatrix.shape[0])
-    print('number of times unsuccesful candidate set usage: ',unsuccesfull_index)
-
     for i in range(Number_Of_Clusters):
         for j in range(np.size(z_i[i])):
             try:
@@ -109,20 +105,17 @@ def local_ecm(Matrixlist, vectorlist, swap_functions, constrain_sum_of_weights )
             except:
                 #single number found
                 WeightsMatrix[z_i[i], i] = w_i[i]
-
     return z, WeightsMatrix
 
 
 
+
+
 def independent_ecms(Matrixlist, vectorlist):
-
     Number_Of_Clusters = len(Matrixlist)
-
     z_i = []
     w_i = []
-
     for i in range(Number_Of_Clusters):
-        print('current index : ', i)
         hyper_reduction_element_selector = EmpiricalCubatureMethod()
         uu, ss, vv, ee = RandomizedSingularValueDecomposition().Calculate(Matrixlist[i].T)
         hyper_reduction_element_selector.SetUp(vv.T, Weights=vectorlist)
@@ -130,21 +123,22 @@ def independent_ecms(Matrixlist, vectorlist):
         hyper_reduction_element_selector.Calculate()
         w_i.append(np.squeeze(hyper_reduction_element_selector.w))
         z_i.append(np.squeeze(hyper_reduction_element_selector.z))
-
     return z_i, w_i
 
 
-def global_ecm(GlobalMatrix, vectorlist):
 
+
+
+def global_ecm(GlobalMatrix, vectorlist):
     integrand = np.block(GlobalMatrix).T
     uu, ss, vv, ee = RandomizedSingularValueDecomposition().Calculate(integrand)
     hyper_reduction_element_selector = EmpiricalCubatureMethod()
-
     hyper_reduction_element_selector.SetUp( vv.T , Weights=vectorlist )
     hyper_reduction_element_selector.Initialize()
     hyper_reduction_element_selector.Calculate()
-
     return hyper_reduction_element_selector.z, hyper_reduction_element_selector.w
+
+
 
 
 
@@ -160,20 +154,19 @@ def function_2(X,power):
 
 
 
-def example_paper_local_ecm(number_of_fucntions, number_of_candidate_Gauss_points, funtion_to_use, swap_functions, constrain_sum_of_weights):
-    n = number_of_fucntions
+def run_example(number_of_functions, number_of_candidate_Gauss_points, function_to_use, swap_functions, constrain_sum_of_weights):
+    n = number_of_functions
     M = number_of_candidate_Gauss_points
-    min_ = 0
-    max_ = 1
+
     FunctionEvaluations = []
     BasisFunctionEvaluations = []
     GroundTruth = []
 
-    [X,W] = gauss_quad(M, min_, max_) #Gauss points and weights as candidates
+    [X,W] = gauss_quad(M) #Gauss points and weights as candidates
 
 
     for i in range(n):
-        if funtion_to_use==1:
+        if function_to_use==1:
             Abar = function_1(X,i)
         else:
             Abar = function_2(X,i)
@@ -206,12 +199,16 @@ def example_paper_local_ecm(number_of_fucntions, number_of_candidate_Gauss_point
     indexes_lp = np.where(res.x > np.zeros(res.x.shape)+1e-6)
     weights_lp = res.x[indexes_lp]
 
+
+
     ### Local ECM
     indexes_local_ecm, weights_local_ecm = local_ecm(FunctionEvaluations, W, swap_functions, constrain_sum_of_weights)
-    plot_spartity_plot(weights_local_ecm.T, 'local_ecm')
+    plot_sparsity(weights_local_ecm.T, 'local_ecm')
     weights_local_ecm = weights_local_ecm.T.reshape(-1)
     indexes_local_ecm = np.where(weights_local_ecm > np.zeros(weights_local_ecm.shape)+1e-6)
     weights_local_ecm = weights_local_ecm[indexes_local_ecm]
+
+
 
 
     ### Multiple indepenent ECMs
@@ -219,7 +216,9 @@ def example_paper_local_ecm(number_of_fucntions, number_of_candidate_Gauss_point
     matrix = np.zeros(matrix.shape)
     for i in range(n):
         matrix[i, index_list_independent_ecms[i]] = 1
-    plot_spartity_plot(matrix, 'multiple_independent')
+    plot_sparsity(matrix, 'multiple_independent')
+
+
 
 
     ### Solving a single globalECM problem
@@ -227,25 +226,19 @@ def example_paper_local_ecm(number_of_fucntions, number_of_candidate_Gauss_point
     matrix = np.zeros(matrix.shape)
     for i in range(len(index_single_ecm)):
         matrix[:, index_single_ecm[i]] = 1
-    plot_spartity_plot(matrix, 'global')
+    plot_sparsity(matrix, 'global')
 
 
-
-
-    # print('\n\nselected elements by linear programming: ', indexes_lp)
-    # print('selected weights by linear programming: ', weights_lp)
-    # print('\n\nselected elements by local ECM: ', indexes_local_ecm)
-    # print('selected weights by local ECM: ', weights_local_ecm)
-    # print('\n\nselected elements by multiple independent ECMs: ', index_list_independent_ecms)
-    # print('selected weights multiple independent local ECM: ', weight_list_independent_ecm)
-    # print('\n\nselected elements by global ECM: ', index_single_ecm)
-    # print('selected weights global ECM: ', weights_single_ecm)
 
     #Check LP approximation
     lp = A[indexes_lp].T@weights_lp
 
+
+
     #Check Local ECM approximation
     local_ecm_approximation = A[indexes_local_ecm].T@weights_local_ecm
+
+
 
     #Check Independent ECMs
     independent_ecm_approximation = []
@@ -255,22 +248,18 @@ def example_paper_local_ecm(number_of_fucntions, number_of_candidate_Gauss_point
         else:
             independent_ecm_approximation = np.r_[independent_ecm_approximation, ( BasisFunctionEvaluations[i].T[:,index_list_independent_ecms[i]]@weight_list_independent_ecm[i].reshape(-1,1)).reshape(-1,1)  ]   #ppend(np.squeeze(A_i[i][index_list_independent_ecms[i]]*weight_list_independent_ecm[i]))
 
+
+
     #Check Global ECM
     integrand = np.block(BasisFunctionEvaluations).T
     global_ecm_approximation = integrand[:,index_single_ecm]@weights_single_ecm
+
+
 
     # print all approximations
     for i in range(n):
         print('exact:', b[i][0], 'local ecm:', local_ecm_approximation[i], '  lp: ', lp[i], 'independent ecms', independent_ecm_approximation[i],'  global ecm', global_ecm_approximation[i])
 
-
-    # print all approximations
-    print('\n\n\n\n\n\n\n\n\n\n\n')
-    print('exact: ', np.squeeze(b))
-    print('lp: ', np.squeeze(lp))
-    print('global ecm: ', np.squeeze(global_ecm_approximation) )
-    print('independent ecms: ', np.squeeze(independent_ecm_approximation))
-    print('local ecm: ', np.squeeze(local_ecm_approximation))
 
     #plotting all approximations
     plot_ground_truth([b, lp, global_ecm_approximation, independent_ecm_approximation,local_ecm_approximation], ['exact', 'lp','global ecm','independent ecms','local ecm'])
@@ -281,11 +270,11 @@ def example_paper_local_ecm(number_of_fucntions, number_of_candidate_Gauss_point
 
 if __name__=='__main__':
 
-    number_of_fucntions = 50
-    number_of_candidate_Gauss_points = 200
+    number_of_functions = 2
+    number_of_candidate_Gauss_points = 20
 
-    funtion_to_use = 2 # 1 or 2
+    function_to_use = 1 # 1 or 2
     swap_functions = False # True or False. This changes the order of the functions to integrate
     constrain_sum_of_weights = True
 
-    example_paper_local_ecm(number_of_fucntions, number_of_candidate_Gauss_points, funtion_to_use, swap_functions, constrain_sum_of_weights)
+    run_example(number_of_functions, number_of_candidate_Gauss_points, function_to_use, swap_functions, constrain_sum_of_weights)
--- plot_scripts.py
+++ plot_scripts.py
@@ -1,54 +1,28 @@
 import numpy as np
 from matplotlib import pyplot as plt
-try:
-    kkk
-    plt.rc('text', usetex=True)
-    plt.rc('font', family='serif')
-    latex_available = True
-except:
-    latex_available = False
-
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 import itertools
 
-# def plot_original_function_1D(X,A_i):
-#     number_of_polynomials = len(A_i)
+#plotting variables
+colors = ['k', 'm', 'g', 'b', 'c', 'r', 'y']
+markers = ['X', '<', '^', '*', 'v', 'P', '>', 'd', '8', 'H']
+line_styles = ['-', '--', '-.', ':']
 
-#     colours = ['k','m','g','b','c','r','y']
-#     markers=['X','<','^','*','v','P','>','d','8','H']
-
-#     counter = 0
-#     for i in range(number_of_polynomials):
-#         plt.plot(X, A_i[i], colours[counter], marker=markers[counter], label=r"$a^{{{}}}(x,{{{}}})$".format(i+1,i))
-#         #plt.plot(X, A_i[i], label=r"$a^{{{}}}(x,{{{}}})$".format(i+1,i))
-#         counter+=1
-#     plt.xlabel(r'$x$', size=15, fontweight='bold')
-#     plt.ylabel(r'$a^{(i+1)}$',size=15,fontweight='bold')
-#     plt.legend()
-#     plt.grid()
-#     plt.savefig(f'1d_functions.pdf')
-#     plt.show()
+color_cycle = itertools.cycle(colors)
+marker_cycle = itertools.cycle(markers)
+line_style_cycle = itertools.cycle(line_styles)
 
 
 
 def plot_ground_truth(solutions, labels):
-
-    colors = ['k', 'm', 'g', 'b', 'c', 'r', 'y']
-    markers = ['X', '<', '^', '*', 'v', 'P', '>', 'd', '8', 'H']
-    line_styles = ['-', '--', '-.', ':']
-
-    color_cycle = itertools.cycle(colors)
-    marker_cycle = itertools.cycle(markers)
-    line_style_cycle = itertools.cycle(line_styles)
-
     number_of_solutions = len(solutions)
-
     for i in range(number_of_solutions):
         color = next(color_cycle)
         marker = next(marker_cycle)
         line_style = next(line_style_cycle)
         plt.plot( solutions[i] , color=color, marker=marker, linestyle=line_style,
                  label=labels[i])
-
     plt.xlabel(r'$x$', size=15, fontweight='bold')
     plt.ylabel(r'$\int a^{(i+1) dx }$', size=15, fontweight='bold')
 
@@ -56,7 +30,6 @@ def plot_ground_truth(solutions, labels):
     plt.rcParams['font.size'] = 8  # Adjust the font size
     # Place the legend outside the plot
     plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
-
     plt.legend()
     plt.grid()
     plt.savefig(f'ground_truth.pdf',bbox_inches='tight')
@@ -68,20 +41,8 @@ def plot_ground_truth(solutions, labels):
 
 
 
-
-
-
-
 def plot_original_function_1D(X, A_i, componenet_to_plot=0):
     number_of_polynomials = len(A_i)
-
-    colors = ['k', 'm', 'g', 'b', 'c', 'r', 'y']
-    markers = ['X', '<', '^', '*', 'v', 'P', '>', 'd', '8', 'H']
-    line_styles = ['-', '--', '-.', ':']
-
-    color_cycle = itertools.cycle(colors)
-    marker_cycle = itertools.cycle(markers)
-    line_style_cycle = itertools.cycle(line_styles)
 
     for i in range(number_of_polynomials):
         color = next(color_cycle)
@@ -107,20 +68,8 @@ def plot_original_function_1D(X, A_i, componenet_to_plot=0):
 
 
 
-
-
-
-
 def plot_basis_functions_1D(X, A_i, componenet_to_plot=0):
     number_of_polynomials = len(A_i)
-
-    colors = ['k', 'm', 'g', 'b', 'c', 'r', 'y']
-    markers = ['X', '<', '^', '*', 'v', 'P', '>', 'd', '8', 'H']
-    line_styles = ['-', '--', '-.', ':']
-
-    color_cycle = itertools.cycle(colors)
-    marker_cycle = itertools.cycle(markers)
-    line_style_cycle = itertools.cycle(line_styles)
 
     for i in range(number_of_polynomials):
         color = next(color_cycle)
@@ -148,6 +97,8 @@ def plot_basis_functions_1D(X, A_i, componenet_to_plot=0):
     plt.show()
 
 
+
+
 def plot_lp_sparsity(lp_matrices,methods, title ):
     for i in range(len(lp_matrices)):
         # Plot the sparse representation (spy) of the matrix
@@ -160,248 +111,16 @@ def plot_lp_sparsity(lp_matrices,methods, title ):
     plt.show()
 
 
-# def plot_basis_functions_1D(X,A_i):
-#     number_of_polynomials = len(A_i)
-
-#     colours = ['k','m','g','b','c','r','y']
-#     markers=['X','<','^','*','v','P','>','d','8','H']
-
-#     counter = 0
-#     for i in range(number_of_polynomials):
-#         plt.plot(X, A_i[i], colours[counter], marker=markers[counter], label=r"$u^{{{}}}(x)$".format(i+1,i))
-#         #plt.plot(X, A_i[i],label=r"$u^{{{}}}(x)$".format(i+1,i))
-#         counter+=1
-#     plt.xlabel(r'$x$', size=15, fontweight='bold')
-#     plt.ylabel(r'$u^{(i+1)}$',size=15,fontweight='bold')
-#     plt.legend()
-#     plt.grid()
-#     plt.savefig(f'1d_basis_functions.pdf')
-#     plt.show()
 
 
 
-
-def plot_spartity_plot(short_and_fat_sparse_matrix, title):
+def plot_sparsity(short_and_fat_sparse_matrix, title):
     plt.spy(short_and_fat_sparse_matrix)
     plt.xlabel(r'$\bar{\textbf{x}}_j$', size=15, fontweight='bold')
     plt.ylabel(r'$i$-th function',size=15,fontweight='bold')
     plt.tight_layout()
     plt.savefig(f'sparsity_{title}.pdf', bbox_inches='tight')
     plt.show()
-
-
-def plot_elements(tol_svd,to_svd_res,resuts_path):
-
-    for svd in tol_svd:
-        print('\n')
-        for res in to_svd_res:
-            nelems = np.load(resuts_path+f'Elementsvector_{svd}_{res}.npy')
-            print('svd tol: ',svd, ' res_tol: ',res, ' number of elements: ', np.size(nelems))
-        print('\n')
-
-
-
-def plot_hysteresis(tol_svd,to_svd_res,resuts_path):
-    markers=['s','^','+','H','*']
-    counter = 0
-    for svd in tol_svd:
-        vy = np.load(resuts_path+'Velocity_y.npy')
-        w = np.load(resuts_path+'narrowing.npy')
-        plt.plot(vy, w, 'b', label = 'FOM', linewidth = 1.5 ) #linewidth = 3, alpha=0.5
-        vy_rom = np.load(resuts_path+f'y_velocity_ROM_{svd}.npy')
-        w_rom = np.load(resuts_path+f'narrowing_ROM_{svd}.npy')
-        plt.plot(vy_rom, w_rom, label = f"ROM\_{svd}", linewidth = 1.5) #alpha=0.5
-        for res in to_svd_res:
-            vy_rom = np.load(resuts_path+f'y_velocity_HROM_{svd}_{res}.npy')
-            w_rom = np.load(resuts_path+f'narrowing_HROM_{svd}_{res}.npy')
-            plt.plot(vy_rom, w_rom, markevery=50,label = f"HROM\_{svd}\_{res}", linewidth = 1.5) #alpha=0.5 marker=markers[counter],
-            counter+=1
-        plt.xlabel(r'$v_y^*$', size=15, fontweight='bold')
-        plt.ylabel(r'$w_c$',size=15,fontweight='bold')
-        plt.legend()
-        plt.grid()
-        plt.show()
-
-    #plt.title(r'FOM vs ROM', fontsize=20, fontweight='bold')
-    # plt.xticks(np.arange(-3.5,0.25,0.25))  #TODO the ticks are not easily beautyfied, do it later :)
-    # plt.yticks(np.arange(0,3.1,0.25))
-    #plt.savefig('fom_vs_rom')
-
-
-def plot_hysteresis_fom_rom(tol_svd,to_svd_res,resuts_path, trajectory):
-    if trajectory == "train":
-        add_to_name = ''
-    elif trajectory == "test":
-        add_to_name = '_test'
-
-    colours = ['k','m','g','b','c','r','y']
-    markers=['X','<','^','*','v','P','>','d','8','H']
-    vy = np.load(resuts_path+f'Velocity_y{add_to_name}.npy')
-    w = np.load(resuts_path+f'narrowing{add_to_name}.npy')
-    plt.plot(vy, w, colours[0], marker=markers[0], markevery=50, label = 'FOM') #linewidth = 3, alpha=0.5
-    #plt.plot(vy, w, colours[0], linewidth = 5, alpha=0.5)
-    counter=1
-    for svd in tol_svd:
-        vy_rom = np.load(resuts_path+f'y_velocity_ROM_{svd}{add_to_name}.npy')
-        w_rom = np.load(resuts_path+f'narrowing_ROM_{svd}{add_to_name}.npy')
-        plt.plot(vy_rom, w_rom, colours[counter], marker=markers[counter], markevery=80+counter*15,label = r"$ROM \ \epsilon_{SOL}=$" + "{:.0e}".format(svd), alpha=0.9) #alpha=0.5, #alpha=0.5
-        counter+=1
-    plt.xlabel(r'$v_y^*$', size=15)
-    plt.ylabel(r'$w_c$',size=15)
-    plt.grid()
-    #plt.xlim([-4,0])
-    plt.legend()
-    plt.savefig(f'hysteresis_fom_vs_rom_{trajectory}.pdf')
-    plt.show()
-
-
-    #plt.title(r'FOM vs ROM', fontsize=20, fontweight='bold')
-    # plt.xticks(np.arange(-3.5,0.25,0.25))  #TODO the ticks are not easily beautyfied, do it later :)
-    # plt.yticks(np.arange(0,3.1,0.25))
-    #plt.savefig('fom_vs_rom')
-
-
-
-def plot_hysteresis_rom_hrom(tol_svd,to_svd_res,resuts_path, trajectory):
-
-    if trajectory == "train":
-        add_to_name = ''
-    elif trajectory == "test":
-        add_to_name = '_test'
-
-    colours = ['k','m','g','b','c','r','y']
-    markers=['X','<','^','*','v','P','>','d','8','H']
-
-    for svd in tol_svd:
-        vy_rom = np.load(resuts_path+f'y_velocity_ROM_{svd}{add_to_name}.npy')
-        w_rom = np.load(resuts_path+f'narrowing_ROM_{svd}{add_to_name}.npy')
-        counter=0
-        plt.plot(vy_rom, w_rom, colours[counter], marker=markers[counter], markevery=50,label = r"$ROM \ \epsilon_{SOL}=$" + "{:.0e}".format(svd), alpha=0.9) #alpha=0.5, #alpha=0.5
-        for res in to_svd_res:
-            vy_rom = np.load(resuts_path+f'y_velocity_HROM{add_to_name}_{svd}_{res}.npy')
-            w_rom = np.load(resuts_path+f'narrowing_HROM{add_to_name}_{svd}_{res}.npy')
-            plt.plot(vy_rom, w_rom, markevery=80+counter*15,
-            label = r"$HROM \ \epsilon_{SOL}=$ " + "{:.0e}".format(svd) + r" ; $\epsilon_{RES}=$ "+"{:.0e}".format(res),
-            linewidth = 1.5, marker=markers[counter]) #alpha=0.5 marker=markers[counter]
-            counter+=1
-        counter+=1
-        plt.xlabel(r'$v_y^*$', size=15)
-        plt.ylabel(r'$w_c$',size=15)
-        plt.xlim([-4,1])
-        plt.grid()
-        plt.legend()
-        plt.savefig(f'hysteresis_rom_vs_hrom_{svd}_{trajectory}.pdf')
-        plt.show()
-
-
-def GetPercentualError(reference, approx):
-    return np.linalg.norm(reference - approx) / np.linalg.norm(reference) *100
-
-
-def plot_errors_FOM_ROM(tol_svd,to_svd_res,resuts_path,trajectory):
-
-    if trajectory == "train":
-        add_to_name = ''
-    elif trajectory == "test":
-        add_to_name = '_test'
-
-    FOM = np.load(resuts_path+f'SnapshotMatrix{add_to_name}.npy')
-    for svd in tol_svd:
-        ROM = np.load(resuts_path+f'ROM_snapshots{add_to_name}_{svd}.npy')
-        print('FOM vs ROM error: ', GetPercentualError(FOM, ROM),'%\n')
-
-
-def plot_errors_ROM_HROM(tol_svd,to_svd_res,resuts_path,trajectory):
-
-    if trajectory == "train":
-        add_to_name = ''
-    elif trajectory == "test":
-        add_to_name = '_test'
-
-    for svd in tol_svd:
-        ROM = np.load(resuts_path+f'ROM_snapshots{add_to_name}_{svd}.npy')
-        print('\n solution_svd_truncation:',svd)
-        for res in to_svd_res:
-            print('residual_svd_truncation:',res)
-            HROM = np.load(resuts_path+f'HROM_snapshots{add_to_name}_{svd}_{res}.npy')
-            print('ROM vs HROM error: ', GetPercentualError(ROM,HROM),'%')
-        print('\n')
-
-
-
-
-
-
-def error_QoI_FOM_vs_ROM(tol_svd,to_svd_res,resuts_path,trajectory):
-
-    if trajectory == "train":
-        add_to_name = ''
-    elif trajectory == "test":
-        add_to_name = '_test'
-
-
-
-    FOM = np.load(resuts_path+f'Velocity_y{add_to_name}.npy')
-    for svd in tol_svd:
-        ROM = np.load(resuts_path+f'y_velocity_ROM{add_to_name}_{svd}.npy')
-        print('FOM vs ROM error: ', GetPercentualError(FOM, ROM),'%\n')
-
-
-
-
-
-def error_QoI_ROM_vs_HROM(tol_svd,to_svd_res,resuts_path,trajectory):
-
-    if trajectory == "train":
-        add_to_name = ''
-    elif trajectory == "test":
-        add_to_name = '_test'
-
-
-
-    for svd in tol_svd:
-        ROM = np.load(resuts_path+f'y_velocity_ROM{add_to_name}_{svd}.npy')
-        print('\nsolution_svd_truncation:',svd)
-        for res in to_svd_res:
-            #print('residual_svd_truncation:',res)
-            HROM = np.load(resuts_path+f'y_velocity_HROM{add_to_name}_{svd}_{res}.npy')
-            print('ROM vs HROM error: ', GetPercentualError(ROM,HROM),'%')
-        print('\n')
-
-
-
-
-def plot_number_of_modes(tol_svd,results_path_rom):
-    for svd in tol_svd:
-        number_of_modes = np.load(results_path_rom+f'{svd}.npy').shape[1]
-        print(f'\nfor a tolerance of {svd}, {number_of_modes} were selected')
-        print('\n')
-
-
-
-
-
-if __name__=='__main__':
-
-    resuts_path_hrom = './HROM/'
-    results_path_rom = './ROM/'
-    resuts_path = './Results/' #'./Results_slightly_shifted/'
-    trajectory = "train" # test or train
-
-    tol_svd = [1e-3,1e-4,1e-5,1e-6]
-    to_svd_res = [1e-3,1e-4,1e-5,1e-6]
-
-    #TODO add testing trajectory plots!!!
-
-    # plot_hysteresis(tol_svd,to_svd_res,resuts_path)
-    # plot_hysteresis_fom_rom(tol_svd,to_svd_res,resuts_path, trajectory)
-    # plot_hysteresis_rom_hrom(tol_svd,to_svd_res,resuts_path, trajectory)
-    # plot_number_of_modes(tol_svd,results_path_rom)
-    # plot_elements(tol_svd,to_svd_res,resuts_path_hrom )
-    plot_errors_FOM_ROM(tol_svd,to_svd_res,resuts_path, trajectory)
-    #plot_errors_ROM_HROM(tol_svd,to_svd_res,resuts_path, trajectory)
-    #error_QoI_FOM_vs_ROM(tol_svd,to_svd_res,resuts_path, trajectory)
-    #error_QoI_ROM_vs_HROM(tol_svd,to_svd_res,resuts_path, trajectory)
 
 
 
