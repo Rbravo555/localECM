0506a9ef6bd64c1ad617d6327b86435e54d2aa14
Raul Bravo <rbravo555@gmail.com>
2023-06-29 17:04:13 +0200

    adding python files

---
+++ Example.py
@@ -0,0 +1,291 @@
+
+import numpy as np
+from scipy.special.orthogonal import p_roots
+from matplotlib import pyplot as plt
+from scipy.optimize import linprog
+from empirical_cubature_method import EmpiricalCubatureMethod
+from randomized_singular_value_decomposition import RandomizedSingularValueDecomposition
+from plot_scripts import plot_original_function_1D, plot_basis_functions_1D, plot_spartity_plot, plot_lp_sparsity,plot_ground_truth
+
+def gauss_quad(M, min_, max_):
+    [X, W] = p_roots(M)
+    dx = max_- min_
+    dxi = 2
+    W = W*(dx/dxi)
+    X = ((max_ - min_)/2)*X + ((max_ + min_)/2)
+
+    return X, W
+
+
+def get_list_of_arrays_sizes(list_of_np_arrays):
+    size_1 = 0
+    size_2 = 0
+    for i in range(len(list_of_np_arrays)):
+        size_1+= list_of_np_arrays[i].shape[0]
+        size_2+= list_of_np_arrays[i].shape[1]
+    return size_1, size_2
+
+def list_to_mat(list_of_np_arrays):
+    #generic but not efficient
+    size_1, size_2= get_list_of_arrays_sizes(list_of_np_arrays)
+    numpy_arr = np.zeros((size_1,size_2))
+
+    gap_horizontal = 0
+    gap_vertical = 0
+    for i in range(len(list_of_np_arrays)):
+        size_1_array = list_of_np_arrays[i].shape[0]
+        size_2_array = list_of_np_arrays[i].shape[1]
+
+        numpy_arr[gap_horizontal:gap_horizontal+size_1_array,gap_vertical:gap_vertical+size_2_array] = list_of_np_arrays[i]
+
+        gap_horizontal += size_1_array
+        gap_vertical +=size_2_array
+
+    return numpy_arr
+
+
+def list_to_vec(list_of_np_arrays):
+    #hardcoded a bit
+    size_2 = len(list_of_np_arrays)
+    for i in range(size_2):
+        if i==0:
+            numpy_arr = list_of_np_arrays[i].reshape(-1,1)
+        else:
+            numpy_arr = np.r_[numpy_arr, list_of_np_arrays[i].reshape(-1,1)]
+    return numpy_arr
+
+
+def local_ecm(Matrixlist, vectorlist, swap_functions, constrain_sum_of_weights ):
+    Number_Of_Clusters = len(Matrixlist)
+    z_i = []
+    w_i = []
+    z = None
+
+    for i in range(Number_Of_Clusters):
+        z_i.append([])
+        w_i.append([])
+
+    unsuccesfull_index = 0
+
+    if swap_functions ==True:
+        Matrixlist = Matrixlist[::-1]
+
+    for i in range(Number_Of_Clusters):
+        print('current z: ', z)
+        print('current index : ', i)
+        hyper_reduction_element_selector = EmpiricalCubatureMethod()
+        uu, ss, vv, ee = RandomizedSingularValueDecomposition().Calculate(Matrixlist[i].T)
+        hyper_reduction_element_selector.SetUp( vv.T, Weights=vectorlist,InitialCandidatesSet = z, constrain_sum_of_weights=constrain_sum_of_weights)  #add again z
+        hyper_reduction_element_selector.Initialize()
+        hyper_reduction_element_selector.Calculate()
+        if not hyper_reduction_element_selector.success:
+            unsuccesfull_index+=1
+            hyper_reduction_element_selector = EmpiricalCubatureMethod()
+            hyper_reduction_element_selector.SetUp(vv.T, Weights=vectorlist, InitialCandidatesSet = None, constrain_sum_of_weights=constrain_sum_of_weights)
+            hyper_reduction_element_selector.Initialize()
+            hyper_reduction_element_selector.Calculate()
+        w_i[i] = np.squeeze(hyper_reduction_element_selector.w)
+        z_i[i] = np.squeeze(hyper_reduction_element_selector.z)
+
+        print(w_i[i])
+        print(z_i[i])
+        if z is None:
+            z = z_i[i]
+        else:
+            z = np.union1d(z,z_i[i])
+
+    if swap_functions ==True:
+        w_i = w_i[::-1]
+        z_i = z_i[::-1]
+
+    WeightsMatrix = np.zeros(( (Matrixlist[0].shape)[0],Number_Of_Clusters))
+    print('\n number of TOTAL elements: ',WeightsMatrix.shape[0])
+    print('number of times unsuccesful candidate set usage: ',unsuccesfull_index)
+
+    for i in range(Number_Of_Clusters):
+        for j in range(np.size(z_i[i])):
+            try:
+                WeightsMatrix[z_i[i][j] , i] = w_i[i][j]
+            except:
+                #single number found
+                WeightsMatrix[z_i[i], i] = w_i[i]
+
+    return z, WeightsMatrix
+
+
+
+def independent_ecms(Matrixlist, vectorlist):
+
+    Number_Of_Clusters = len(Matrixlist)
+
+    z_i = []
+    w_i = []
+
+    for i in range(Number_Of_Clusters):
+        print('current index : ', i)
+        hyper_reduction_element_selector = EmpiricalCubatureMethod()
+        uu, ss, vv, ee = RandomizedSingularValueDecomposition().Calculate(Matrixlist[i].T)
+        hyper_reduction_element_selector.SetUp(vv.T, Weights=vectorlist)
+        hyper_reduction_element_selector.Initialize()
+        hyper_reduction_element_selector.Calculate()
+        w_i.append(np.squeeze(hyper_reduction_element_selector.w))
+        z_i.append(np.squeeze(hyper_reduction_element_selector.z))
+
+    return z_i, w_i
+
+
+def global_ecm(GlobalMatrix, vectorlist):
+
+    integrand = np.block(GlobalMatrix).T
+    uu, ss, vv, ee = RandomizedSingularValueDecomposition().Calculate(integrand)
+    hyper_reduction_element_selector = EmpiricalCubatureMethod()
+
+    hyper_reduction_element_selector.SetUp( vv.T , Weights=vectorlist )
+    hyper_reduction_element_selector.Initialize()
+    hyper_reduction_element_selector.Calculate()
+
+    return hyper_reduction_element_selector.z, hyper_reduction_element_selector.w
+
+
+
+def function_1(X,power):
+    return (X**(power)).reshape(-1,1)
+
+
+
+
+def function_2(X,power):
+    return np.c_[np.ones(X.shape), X**(power)]
+
+
+
+
+def example_paper_local_ecm(number_of_fucntions, number_of_candidate_Gauss_points, funtion_to_use, swap_functions, constrain_sum_of_weights):
+    n = number_of_fucntions
+    M = number_of_candidate_Gauss_points
+    min_ = 0
+    max_ = 1
+    FunctionEvaluations = []
+    BasisFunctionEvaluations = []
+    GroundTruth = []
+
+    [X,W] = gauss_quad(M, min_, max_) #Gauss points and weights as candidates
+
+
+    for i in range(n):
+        if funtion_to_use==1:
+            Abar = function_1(X,i)
+        else:
+            Abar = function_2(X,i)
+        FunctionEvaluations.append(Abar)
+        AbarW = np.sqrt(W).reshape(-1,1)* Abar
+        uu, ss, vv, ee = RandomizedSingularValueDecomposition().Calculate(AbarW)
+        BasisFunctionEvaluations.append(uu*(1/np.sqrt(W.reshape(-1,1))))
+        GroundTruth.append(BasisFunctionEvaluations[i].T@W)
+
+    plot_original_function_1D(X,FunctionEvaluations)
+    plot_basis_functions_1D(X,BasisFunctionEvaluations)
+
+
+    A = list_to_mat(BasisFunctionEvaluations)
+    b = list_to_vec(GroundTruth)
+    c = np.ones(M*n)
+
+
+    ### LP
+    lp_matrices = []
+    methods = ['highs' , 'highs-ds', 'highs-ipm', 'interior-point' , 'revised simplex' , 'simplex', '']
+    for i in range(6):
+        res = linprog(c, A_eq=A.T, b_eq=b, bounds=(0, None), method=methods[i] )
+        #this makes the values very close to zero dissapear
+        indexes_lp = np.where(res.x < np.zeros(res.x.shape)+1e-8)[0]
+        res.x [indexes_lp]=0
+        matrix = (res.x).reshape(n,M)
+        lp_matrices.append(matrix)
+    plot_lp_sparsity(lp_matrices,methods,'sparsity_linear_programming')
+    indexes_lp = np.where(res.x > np.zeros(res.x.shape)+1e-6)
+    weights_lp = res.x[indexes_lp]
+
+    ### Local ECM
+    indexes_local_ecm, weights_local_ecm = local_ecm(FunctionEvaluations, W, swap_functions, constrain_sum_of_weights)
+    plot_spartity_plot(weights_local_ecm.T, 'local_ecm')
+    weights_local_ecm = weights_local_ecm.T.reshape(-1)
+    indexes_local_ecm = np.where(weights_local_ecm > np.zeros(weights_local_ecm.shape)+1e-6)
+    weights_local_ecm = weights_local_ecm[indexes_local_ecm]
+
+
+    ### Multiple indepenent ECMs
+    index_list_independent_ecms, weight_list_independent_ecm  = independent_ecms(FunctionEvaluations, W)
+    matrix = np.zeros(matrix.shape)
+    for i in range(n):
+        matrix[i, index_list_independent_ecms[i]] = 1
+    plot_spartity_plot(matrix, 'multiple_independent')
+
+
+    ### Solving a single globalECM problem
+    index_single_ecm, weights_single_ecm  = global_ecm(FunctionEvaluations, W)
+    matrix = np.zeros(matrix.shape)
+    for i in range(len(index_single_ecm)):
+        matrix[:, index_single_ecm[i]] = 1
+    plot_spartity_plot(matrix, 'global')
+
+
+
+
+    # print('\n\nselected elements by linear programming: ', indexes_lp)
+    # print('selected weights by linear programming: ', weights_lp)
+    # print('\n\nselected elements by local ECM: ', indexes_local_ecm)
+    # print('selected weights by local ECM: ', weights_local_ecm)
+    # print('\n\nselected elements by multiple independent ECMs: ', index_list_independent_ecms)
+    # print('selected weights multiple independent local ECM: ', weight_list_independent_ecm)
+    # print('\n\nselected elements by global ECM: ', index_single_ecm)
+    # print('selected weights global ECM: ', weights_single_ecm)
+
+    #Check LP approximation
+    lp = A[indexes_lp].T@weights_lp
+
+    #Check Local ECM approximation
+    local_ecm_approximation = A[indexes_local_ecm].T@weights_local_ecm
+
+    #Check Independent ECMs
+    independent_ecm_approximation = []
+    for i in range(n):
+        if i ==0:
+            independent_ecm_approximation = (BasisFunctionEvaluations[i].T[:,index_list_independent_ecms[i]]@ weight_list_independent_ecm[i].reshape(-1,1)).reshape(-1,1)
+        else:
+            independent_ecm_approximation = np.r_[independent_ecm_approximation, ( BasisFunctionEvaluations[i].T[:,index_list_independent_ecms[i]]@weight_list_independent_ecm[i].reshape(-1,1)).reshape(-1,1)  ]   #ppend(np.squeeze(A_i[i][index_list_independent_ecms[i]]*weight_list_independent_ecm[i]))
+
+    #Check Global ECM
+    integrand = np.block(BasisFunctionEvaluations).T
+    global_ecm_approximation = integrand[:,index_single_ecm]@weights_single_ecm
+
+    # print all approximations
+    for i in range(n):
+        print('exact:', b[i][0], 'local ecm:', local_ecm_approximation[i], '  lp: ', lp[i], 'independent ecms', independent_ecm_approximation[i],'  global ecm', global_ecm_approximation[i])
+
+
+    # print all approximations
+    print('\n\n\n\n\n\n\n\n\n\n\n')
+    print('exact: ', np.squeeze(b))
+    print('lp: ', np.squeeze(lp))
+    print('global ecm: ', np.squeeze(global_ecm_approximation) )
+    print('independent ecms: ', np.squeeze(independent_ecm_approximation))
+    print('local ecm: ', np.squeeze(local_ecm_approximation))
+
+    #plotting all approximations
+    plot_ground_truth([b, lp, global_ecm_approximation, independent_ecm_approximation,local_ecm_approximation], ['exact', 'lp','global ecm','independent ecms','local ecm'])
+
+
+
+
+
+if __name__=='__main__':
+
+    number_of_fucntions = 50
+    number_of_candidate_Gauss_points = 200
+
+    funtion_to_use = 2 # 1 or 2
+    swap_functions = False # True or False. This changes the order of the functions to integrate
+    constrain_sum_of_weights = True
+
+    example_paper_local_ecm(number_of_fucntions, number_of_candidate_Gauss_points, funtion_to_use, swap_functions, constrain_sum_of_weights)
---
+++ empirical_cubature_method.py
@@ -0,0 +1,264 @@
+import numpy as np
+import json
+
+try:
+    from matplotlib import pyplot as plt
+    missing_matplotlib = False
+except ImportError as e:
+    missing_matplotlib = True
+
+
+class EmpiricalCubatureMethod():
+    """
+    This class selects a subset of elements and corresponding positive weights necessary for the construction of a hyper-reduced order model
+    Reference: Hernandez 2020. "A multiscale method for periodic structures using domain decomposition and ECM-hyperreduction"
+    """
+
+
+
+    def __init__(
+        self,
+        ECM_tolerance = 0,
+        Filter_tolerance = 1e-16,
+        Plotting = False,
+        MaximumNumberUnsuccesfulIterations = 100
+    ):
+        """
+        Constructor setting up the parameters for the Element Selection Strategy
+            ECM_tolerance: approximation tolerance for the element selection algorithm
+            Filter_tolerance: parameter limiting the number of candidate points (elements) to those above this tolerance
+            Plotting: whether to plot the error evolution of the element selection algorithm
+        """
+        self.ECM_tolerance = ECM_tolerance
+        self.Filter_tolerance = Filter_tolerance
+        self.Name = "EmpiricalCubature"
+        self.Plotting = Plotting
+        self.MaximumNumberUnsuccesfulIterations = MaximumNumberUnsuccesfulIterations
+
+    def SetUp(
+        self,
+        G,
+        Weights,
+        constrain_sum_of_weights = False,
+        InitialCandidatesSet = None,
+    ):
+        """
+        Method for setting up the element selection
+        input:  ResidualsBasis: numpy array containing a basis to the residuals projected
+        """
+        self.W = Weights
+        self.G = G
+        self.y = InitialCandidatesSet
+        self.add_constrain_count = None
+        if constrain_sum_of_weights:
+            #This avoids the trivial solution w = 0
+            constant_function = np.ones(np.shape(self.G)[1])
+            projection_of_constant_function_on_col_G = constant_function - self.G.T@( self.G @ constant_function)
+            if np.linalg.norm(projection_of_constant_function_on_col_G)>1e-10:
+                projection_of_constant_function_on_col_G/= np.linalg.norm(projection_of_constant_function_on_col_G)
+                self.G = np.vstack([ self.G , projection_of_constant_function_on_col_G] )
+                self.add_constrain_count = -1
+        self.b = self.G @ self.W
+        self.UnsuccesfulIterations = 0
+
+
+
+    def Initialize(self):
+        """
+        Method performing calculations required before launching the Calculate method
+        """
+        self.Gnorm = np.linalg.norm(self.G[:self.add_constrain_count,:], axis = 0)#np.sqrt(sum(np.multiply(self.G, self.G), 0))
+        #aaaa = np.sqrt(sum(np.multiply(self.G[:self.add_constrain_count,:], self.G[:self.add_constrain_count,:]), 0))
+        M = np.shape(self.G)[1]
+        normB = np.linalg.norm(self.b)
+
+        if self.y is None:
+            self.y = np.arange(0,M,1) # Set of candidate points (those whose associated column has low norm are removed)
+
+            if self.Filter_tolerance > 0:
+                TOL_REMOVE = self.Filter_tolerance * normB
+                rmvpin = np.where(self.Gnorm[self.y] < TOL_REMOVE)
+                self.y_complement = self.y[rmvpin] #TODO add this? Joaquin has it
+                self.y = np.delete(self.y,rmvpin)
+        else:
+            self.y_complement = np.arange(0,M,1)
+            self.y_complement = np.delete(self.y_complement, self.y)# Set of candidate points (those whose associated column has low norm are removed)
+            if self.Filter_tolerance > 0:
+                TOL_REMOVE = self.Filter_tolerance * normB
+                rmvpin = np.where(self.Gnorm[self.y_complement] < TOL_REMOVE)
+                self.y_complement = np.delete(self.y_complement,rmvpin)
+
+        self.z = {}  # Set of intergration points
+        self.mPOS = 0 # Number of nonzero weights
+        self.r = self.b.copy() # residual vector
+        print(self.r.shape)
+        self.m = len(self.b) # Default number of points
+        self.nerror = np.linalg.norm(self.r)/normB
+        self.nerrorACTUAL = self.nerror
+
+
+    def Run(self):
+        """
+        Method launching the element selection algorithm to find a set of elements: self.z, and weights: self.w
+        """
+        self.Initialize()
+        self.Calculate()
+
+    def expand_candidates_with_complement(self):
+        self.y = np.union1d(self.y, self.y_complement)
+        print('expanding set to include the complement...')
+        ExpandedSetFlag = True
+        return ExpandedSetFlag
+
+
+    def Calculate(self):
+        """
+        Method calculating the elements and weights, after the Initialize method was performed
+        """
+        MaximumLengthZ = 0
+        ExpandedSetFlag = False
+        k = 1 # number of iterations
+        self.success = True
+        while self.nerrorACTUAL > self.ECM_tolerance and self.mPOS < self.m and np.size(self.y) != 0:
+
+            if  self.UnsuccesfulIterations >  self.MaximumNumberUnsuccesfulIterations and not ExpandedSetFlag:
+                ExpandedSetFlag = self.expand_candidates_with_complement()
+
+            #Step 1. Compute new point
+            if isinstance(self.y, np.int64) or isinstance(self.y, np.int32):
+                indSORT = 0
+                i = self.y
+            else:
+                ObjFun = self.G[:,self.y].T @ self.r.T
+                ObjFun = ObjFun.T / self.Gnorm[self.y]
+                indSORT = np.argmax(ObjFun)
+                i = self.y[indSORT]
+            if k==1:
+                alpha = np.linalg.lstsq(self.G[:, [i]], self.b)[0]
+                H = 1/(self.G[:,i] @ self.G[:,i].T)
+            else:
+                H, alpha = self._UpdateWeightsInverse(self.G[:,self.z],H,self.G[:,i],alpha)
+
+            #Step 3. Move i from set y to set z
+            if k == 1:
+                self.z = i
+            else:
+                self.z = np.r_[self.z,i]
+
+            if isinstance(self.y, np.int64) or isinstance(self.y, np.int32):
+                self.expand_candidates_with_complement()
+                self.y = np.delete(self.y,indSORT)
+            else:
+                self.y = np.delete(self.y,indSORT)
+
+            # Step 4. Find possible negative weights
+            if any(alpha < 0):
+                print("WARNING: NEGATIVE weight found")
+                indexes_neg_weight = np.where(alpha <= 0.)[0]
+                self.y = np.append(self.y, (self.z[indexes_neg_weight]).T)
+                self.z = np.delete(self.z, indexes_neg_weight)
+                H = self._MultiUpdateInverseHermitian(H, indexes_neg_weight)
+                alpha = H @ (self.G[:, self.z].T @ self.b)
+                alpha = alpha.reshape(len(alpha),1)
+
+
+            if np.size(self.z) > MaximumLengthZ :
+                self.UnsuccesfulIterations = 0
+            else:
+                self.UnsuccesfulIterations += 1
+
+            #Step 6 Update the residual
+            if np.size(alpha)==1:
+                self.r = self.b.reshape(-1,1) - (self.G[:,self.z] * alpha).reshape(-1,1)
+                self.r = np.squeeze(self.r)
+            else:
+                Aux = self.G[:,self.z] @ alpha
+                self.r = np.squeeze(self.b - Aux.T)
+            self.nerror = np.linalg.norm(self.r) / np.linalg.norm(self.b)  # Relative error (using r and b)
+            self.nerrorACTUAL = self.nerror
+
+
+            # STEP 7
+            self.mPOS = np.size(self.z)
+            print(f'k = {k}, m = {np.size(self.z)}, error n(res)/n(b) (%) = {self.nerror*100},  Actual error % = {self.nerrorACTUAL*100} ')
+
+            if k == 1:
+                ERROR_GLO = np.array([self.nerrorACTUAL])
+                NPOINTS = np.array([np.size(self.z)])
+            else:
+                ERROR_GLO = np.c_[ ERROR_GLO , self.nerrorACTUAL]
+                NPOINTS = np.c_[ NPOINTS , np.size(self.z)]
+
+            MaximumLengthZ = max(MaximumLengthZ, np.size(self.z))
+            k = k+1
+
+            if k>1000:
+                """
+                this means using the initial candidate set, it was impossible to obtain a set of positive weights.
+                Try again without constraints!!!
+                TODO: incorporate this into greater workflow
+                """
+                self.success = False
+                break
+
+
+        self.w = alpha #* np.sqrt(self.W[self.z])
+
+        print(f'Total number of iterations = {k}')
+
+        if missing_matplotlib == False and self.Plotting == True:
+            plt.plot(NPOINTS[0], ERROR_GLO[0])
+            plt.title('Element Selection Error Evolution')
+            plt.xlabel('Number of elements')
+            plt.ylabel('Error %')
+            plt.show()
+
+
+    def _UpdateWeightsInverse(self, A,Aast,a,xold):
+        """
+        Method for the cheap update of weights (self.w), whenever a negative weight is found
+        """
+        c = np.dot(A.T, a)
+        d = np.dot(Aast, c).reshape(-1, 1)
+        s = np.dot(a.T, a) - np.dot(c.T, d)
+        aux1 = np.hstack([Aast + np.outer(d, d) / s, -d / s])
+        if np.shape(-d.T / s)[1]==1:
+            s = s.reshape(1,-1)
+            aux2 = np.squeeze(np.hstack([-d.T / s, 1 / s]))
+        else:
+            aux2 = np.hstack([np.squeeze(-d.T / s), 1 / s])
+        Bast = np.vstack([aux1, aux2])
+        v = np.dot(a.T, self.r) / s
+        x = np.vstack([(xold - d * v), v])
+        return Bast, x
+
+
+    def _MultiUpdateInverseHermitian(self, invH, neg_indexes):
+        """
+        Method for the cheap update of weights (self.w), whenever a negative weight is found
+        """
+        neg_indexes = np.sort(neg_indexes)
+        for i in range(np.size(neg_indexes)):
+            neg_index = neg_indexes[i] - i
+            invH = self._UpdateInverseHermitian(invH, neg_index)
+        return invH
+
+
+    def _UpdateInverseHermitian(self, invH, neg_index):
+        """
+        Method for the cheap update of weights (self.w), whenever a negative weight is found
+        """
+        if neg_index == np.shape(invH)[1]:
+            aux = (invH[0:-1, -1] * invH[-1, 0:-1]) / invH(-1, -1)
+            invH_new = invH[:-1, :-1] - aux
+        else:
+            aux1 = np.hstack([invH[:, 0:neg_index], invH[:, neg_index + 1:], invH[:, neg_index].reshape(-1, 1)])
+            aux2 = np.vstack([aux1[0:neg_index, :], aux1[neg_index + 1:, :], aux1[neg_index, :]])
+            invH_new = aux2[0:-1, 0:-1] - np.outer(aux2[0:-1, -1], aux2[-1, 0:-1]) / aux2[-1, -1]
+        return invH_new
+
+
+
+
+
+
