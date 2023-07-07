034143e1de398bfc8be8786356bb452ebf0e33f7
Raul Bravo <rbravo555@gmail.com>
2023-07-07 19:23:30 +0200

    Improvement in plots

--- plot_scripts.py
+++ plot_scripts.py
@@ -4,34 +4,59 @@ plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 import itertools
 
-#plotting variables
-colors = ['k', 'm', 'g', 'b', 'c', 'r', 'y']
-markers = ['X', '<', '^', '*', 'v', 'P', '>', 'd', '8', 'H']
-line_styles = ['-', '--', '-.', ':']
-
-color_cycle = itertools.cycle(colors)
-marker_cycle = itertools.cycle(markers)
-line_style_cycle = itertools.cycle(line_styles)
 
 
+def start_colours_counters():
+    #plotting variables
+    colors = ['k', 'm', 'g', 'b', 'c', 'r', 'y']
+    markers = ['X', '<', '^', '*', 'v', 'P', '>', 'd', '8', 'H']
+    line_styles = ['-', '--', '-.', ':']
 
-def plot_ground_truth(solutions, labels):
-    number_of_solutions = len(solutions)
-    for i in range(number_of_solutions):
+    return itertools.cycle(colors), itertools.cycle(markers),  itertools.cycle(line_styles)
+
+
+
+def plot_selected_points_histogram(dictionary_of_sparse_solutions):
+
+    # Extract keys and values from the dictionary
+    keys = list(dictionary_of_sparse_solutions.keys())
+    values = list(dictionary_of_sparse_solutions.values())
+
+    # Set y-axis ticks at integer intervals
+    plt.locator_params(axis='y', integer=True)
+    plt.grid()
+
+    # Plotting the histogram
+    plt.bar(keys, values, color='k')
+
+    # Customizing the plot
+    plt.ylabel(r'Number of points selected', fontsize=16)
+
+    # Displaying the plot
+    plt.savefig(f'histogram_points_per_method.pdf',bbox_inches='tight')
+    plt.show()
+
+
+def plot_approximations(data, labels, ylabel, xlabel,  saving_title):
+
+    color_cycle,marker_cycle,line_style_cycle =  start_colours_counters()
+
+    for i in range(len(data)):
         color = next(color_cycle)
         marker = next(marker_cycle)
         line_style = next(line_style_cycle)
-        plt.plot( solutions[i] , color=color, marker=marker, linestyle=line_style,
+        plt.plot( data[i] , color=color, marker=marker, linestyle=line_style,
                  label=labels[i])
-    plt.xlabel(r'$x$', size=15, fontweight='bold')
-    plt.ylabel(r'$\int a^{(i+1) dx }$', size=15, fontweight='bold')
+
+    plt.xlabel(xlabel, size=15, fontweight='bold')
+    plt.ylabel(ylabel, size=15, fontweight='bold')
 
     plt.xticks(rotation='vertical')  # Rotate x-axis tick labels
     plt.rcParams['font.size'] = 8  # Adjust the font size
     plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.legend()
     plt.grid()
-    plt.savefig(f'ground_truth.pdf',bbox_inches='tight')
+    plt.savefig(f'{saving_title}.pdf',bbox_inches='tight')
     plt.show()
 
 
@@ -42,7 +67,7 @@ def plot_ground_truth(solutions, labels):
 
 def plot_original_function_1D(X, A_i, component_to_plot=0):
     number_of_polynomials = len(A_i)
-
+    color_cycle,marker_cycle,line_style_cycle =  start_colours_counters()
     for i in range(number_of_polynomials):
         color = next(color_cycle)
         marker = next(marker_cycle)
@@ -72,7 +97,7 @@ def plot_original_function_1D(X, A_i, component_to_plot=0):
 
 def plot_basis_functions_1D(X, A_i, component_to_plot=0):
     number_of_polynomials = len(A_i)
-
+    color_cycle,marker_cycle,line_style_cycle =  start_colours_counters()
     for i in range(number_of_polynomials):
         color = next(color_cycle)
         marker = next(marker_cycle)
